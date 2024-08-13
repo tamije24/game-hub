d7e4bbe16d1984b60f029e6b8f147ec00201b3c2
Tamije <tamije@pearlabs.com>
2024-08-13 11:36:15 +0530

    Filter games by platforms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,8 +15,18 @@ export interface Game {
     metacritic: number;
   }
 
-const useGames = (selectedGenre: Genre | null) => {
-  const {data, error, isLoading} =   useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => {
+  const {data, error, isLoading} =  
+      useData<Game>(
+        '/games', 
+        {
+          params: {
+            genres: selectedGenre?.id, 
+            platforms: selectedPlatform?.id
+          }
+        }, 
+        [selectedGenre?.id, selectedPlatform?.id]
+      )
   return {data, error, isLoading}
 }
 
