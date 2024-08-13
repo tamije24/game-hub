134a672438f688265206e86417093a4f2d205c98
Tamije <tamije@pearlabs.com>
2024-08-13 05:53:46 +0530

    Display Critic Score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { Text, SimpleGrid, Card } from "@chakra-ui/react";
+import { Text, SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 
