d472a6811e11cc8199b3ed25c5fe9805e219aea9
Tamije <tamije@pearlabs.com>
2024-08-13 19:22:54 +0530

    Ship genres with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+//import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
     id: number;
@@ -6,9 +7,13 @@ export interface Genre {
     image_background: string;
 }
 
-const useGenres = () => {
-    const {data, error, isLoading} =  useData<Genre>("/genres")
-    return {data, error, isLoading}
-}
+// THIS IMPLEMENTATION IS FOR GETTING GENRES FROM SERVER
+// const useGenres = () => {
+//     const {data, error, isLoading} =  useData<Genre>("/genres")
+//     return {data, error, isLoading}
+// }
+
+// THIS IMPLEMETATION IS FOR READING GENRES FROM A STORED STATIC DATA
+const useGenres = () => ({data: genres, error: null, isLoading: false} )
 
 export default useGenres;
