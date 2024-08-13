d24069d2a1935dc8e1e169e61b17239a5d4a1699
Tamije <tamije@pearlabs.com>
2024-08-13 10:11:26 +0530

    Display Genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,11 +2,13 @@ import useData from "./useData";
 
 export interface Genre {
     id: number;
-    name: string
+    name: string;
+    image_background: string;
 }
 
 const useGenres = () => {
     const {data, error, isLoading} =  useData<Genre>("/genres")
     return {data, error, isLoading}
 }
+
 export default useGenres;
